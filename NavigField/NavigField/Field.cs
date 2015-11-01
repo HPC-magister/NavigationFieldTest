f118ac3889d1deb3ae2f301761a043081037acd4
unknown <pershutkin.alexei@.com>
2015-11-01 22:43:36 +0300

    Projects' files are added.

---
+++ NavigField/NavigField/Field.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NavigField
+{
+    class Field
+    {
+        int x;
+        int y;
+        int cost;
+    }
+}
+
---
+++ NavigField/NavigField/GuidanseField.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NavigField
+{
+    class GuidanseField : Field
+    {
+    }
+}
